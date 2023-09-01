145186ab9a55a49ca6176acf3d5a638af73bb7df
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-01 19:58:39 +0200

    Updated logic of the component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div className='expense-item'>
-      <div>March 25th 2021</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.56</div>
+    <div className="expense-item">
+      <ExpenseDate date = {props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
