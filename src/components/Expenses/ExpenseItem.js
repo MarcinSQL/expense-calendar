692ae840a6d72745811d69f775a403b3d9b8722c
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-05 22:34:00 +0200

    Removed unnecessary expense item button and added useState to form inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 //useState is a function to tell react to recall component
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
@@ -7,20 +7,20 @@ import Card from '../UI/Card';
 
 function ExpenseItem(props) {
   //react hook (the important one)
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Updated!');
-  }
+  // const clickHandler = () => {
+  //   setTitle('Updated!');
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,49 @@
+import React, { useState } from "react";
+
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>
             Title
-            <input type="text"></input>
+            <input type="text" onChange={titleChangeHandler} />
           </label>
           <label>
             Amount
-            <input type="number" min="0.01" step="0.01"></input>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={amountChangeHandler}
+            />
           </label>
           <label>
             Date
-            <input type="date" min="2020-01-01" max="2024-12-31"></input>
+            <input
+              type="date"
+              min="2020-01-01"
+              max="2024-12-31"
+              onChange={dateChangeHandler}
+            />
           </label>
         </div>
       </div>
